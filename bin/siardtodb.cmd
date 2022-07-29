bae22f8a5e10508c4e61731108e9757ff3ee8633
Max Burri <burri@puzzle.ch>
2022-07-29 10:07:06 +0200

    use packaged jre for scripts

--- bin/siardtodb.cmd
+++ bin/siardtodb.cmd
@@ -6,6 +6,7 @@ rem  Platform   : Win32
 rem --------------------------------------------------------------------
 rem Copyright  : Swiss Federal Archives, Berne, Switzerland, 2008, 2011, 2016
 rem Created    : 22.09.2016, Simon Jutz
+rem Updated    : 2022-07-29, Max Burri, Yanick Minder, Puzzle ITC
 rem ====================================================================
 chcp 1252
 if "%~1"=="-?" goto help
@@ -15,89 +16,6 @@ if "%~1"=="/h" goto help
 set jar=lib\siardcmd.jar
 set logprop=etc\logging.properties
 set class=ch.admin.bar.siard2.cmd.SiardToDb
-goto start
-
-:regquery
-rem --------------------------------------------------------------------
-rem query the registry
-rem input: %reg%, %key% and %value%
-rem output: %result%
-rem --------------------------------------------------------------------
-rem @echo +regquery(key: %key%, value: %value%)
-set result=
-rem skip to line 4 and get all tokens from 3 (each line has %%f=%VALUE% %%g=<type> %%h=<data>) 
-for /f "skip=4 tokens=1,2,*" %%f in ('%reg% query "%key%" /v %value%') do set result=%%~h
-if not "%result%"=="" goto result
-rem skip to line 4 and get all tokens from 3 (each line has %%f=%VALUE% %%g=<type> %%h=<data>) 
-for /f "skip=2 tokens=1,2,*" %%f in ('%reg% query "%key%" /v %value%') do set result=%%~h
-:result
-rem @echo -regquery: %result%
-goto exit
-
-:getmajor
-rem --------------------------------------------------------------------
-rem extract the major version from version string %1
-rem output: %major%
-rem see https://ss64.com/nt/syntax-replace.html
-rem --------------------------------------------------------------------
-rem @echo +getmajor(%1)
-set major=%1
-rem drop leading "1." from %major%
-if not "%major:~0,2%"=="1." goto maj
-set major=%major:~2%
-:maj
-set tail=%major:*.=%
-call set major=%%major:.%tail%=%%
-rem @echo -getmajor: '%major%'
-goto exit
-
-:regjava
-rem --------------------------------------------------------------------
-rem check registry HKLM\SOFTWARE\JavaSoft\Java Runtime Environment\xxx
-rem for JavaHome\bin\%executable%
-rem input: %reg%, %softkey%, %minversion% and %executable%
-rem output: %jh%
-rem --------------------------------------------------------------------
-rem @echo +regjava(reg: %reg%, softkey: %softkey%, executable: %executable%, minversion: %minversion%)
-set jh=
-rem for JAVA 9 they changed the registry location from "Java Runtime Environment" to "JDK" or "JRE"!
-set key=%softkey%\JavaSoft\JDK
-set value=CurrentVersion
-call :regquery
-if not "%result%"=="" goto current
-set key=%softkey%\JavaSoft\JRE
-set value=CurrentVersion
-call :regquery
-if not "%result%"=="" goto current
-set key=%softkey%\JavaSoft\Java Runtime Environment
-set value=CurrentVersion
-call :regquery
-:current
-rem @echo current version: %result%
-call :getmajor %result%
-set version=%major%
-rem @echo comparing %version% and %minversion%
-if %version% LSS %minversion% goto error
-if "%result%"=="" goto jh
-set cv=%result%
-if "%cv%"=="" goto jh
-set key=%key%\%cv%
-set value=JavaHome
-call :regquery
-if "%result%"=="" goto jh
-set jh=%result%
-rem @echo JAVA_HOME: %jh%
-:jh
-rem @echo -regjava: '%jh%'
-goto exit
-
-:start
-rem --------------------------------------------------------------------
-rem use C:\Windows\system32\reg.exe if it exists
-rem --------------------------------------------------------------------
-set reg=reg.exe
-set sysreg=%SystemRoot%\system32\%reg%
-if exist %sysreg% set reg=%sysreg%
 
 rem --------------------------------------------------------------------
 rem execution directory from which cmd is called
@@ -109,55 +27,15 @@ set execdir=%execdir:~0,-1%
 rem --------------------------------------------------------------------
 rem local variables
 rem --------------------------------------------------------------------
-set executable=java.exe
-set java=
+set java=jre\bin\javaw.exe
 set args=%*
-set softkey=HKLM\Software
-set minversion=1.8
-call :getmajor %minversion%
-set minversion=%major%
 
-:regcheck
-rem @echo check registry for JAVA_HOME
-rem --------------------------------------------------------------------
-rem search for JavaHome in registry
-rem --------------------------------------------------------------------
-call :regjava
-if "%jh%"=="" goto javacheck
-set java=%jh%\bin\%executable%
-if exist "%java%" goto execute
-rem @echo File "%java%" could not be found!
-goto javacheck
-
-:javacheck
-rem --------------------------------------------------------------------
-rem check environment variable JAVA_HOME for %executable%
-rem --------------------------------------------------------------------
-rem @echo checking JAVA_HOME
-if "%JAVA_HOME%"=="" goto error
-call :getmajor %JAVA_HOME:*jdk=%
-set version=%major%
-rem @echo comparing %version% and %minversion%
-if %version% LSS %minversion% goto error
-set java=%JAVA_HOME%\bin\%executable%
-if exist "%java%" goto execute
-rem @echo File "%java%" could not be found!
-goto error
-
-:execute
 rem --------------------------------------------------------------------
 rem execute bin/SiardToDb
 rem --------------------------------------------------------------------
 set opts="-Xmx1024m" "-Djava.util.logging.config.file=%execdir%\%logprop%" %JAVA_OPTS%
 "%java%" %opts% -cp "%execdir%\%jar%" %class% %args%
 goto exit
-
-:error
-rem --------------------------------------------------------------------
-rem error message for missing %executable%
-rem --------------------------------------------------------------------
-@echo "No valid %executable% could be found.                                  "
-@echo "Install the JAVA JRE or indicate correct path on the command line.  "
 
 :help
 rem --------------------------------------------------------------------
--- bin/siardtodb.sh
+++ bin/siardtodb.sh
@@ -20,91 +20,7 @@ rellogprop=etc/logging.properties
 # class with main() to be run
 class=ch.admin.bar.siard2.cmd.SiardToDb
 
-#-----------------------------------------------------------------------
-# javafind searches for java executable. If one is found, 1 is returned
-# and the variable java is set to the full path. Otherwise 0 is returned.
-#-----------------------------------------------------------------------
-javafind()
-{
-  found=0
-  java=$(type -p java)
-  if [ -n "$java" ] 
-  then
-    found=1
-  elif [ -n "$JAVA_HOME" ] && [ -x "$JAVA_HOME/bin/java" ]
-  then
-    $java="$JAVA_HOME/bin/java"
-    found=1
-  fi  
-  return $found 
-} # javafind
-
-#-----------------------------------------------------------------------
-# javackeck returns 1, if $java exists and has major version 
-# $minjavaversion or higher.
-#-----------------------------------------------------------------------
-javacheck()
-{
-  ok=0
-  # N.B.: After "1.8..." comes "9..." - the leading "1." was dropped!
-  # drop "1." from minjavaversion
-  minjavaversion=${minjavaversion#1.}
-  # execute java -version with small memory requirement
-  version=`$java -Xms32M -Xmx32M -version 2>&1`
-  # output must start with something like 'openjdk version "1.8.0_144"' or 'java version "10.0.1"'
-  reQuoted='version "([^"]+)"'
-  if [[ $version =~ $reQuoted ]]
-  then
-    version=${BASH_REMATCH[1]}
-    # drop "1." from version
-    version=${version#1.}
-    # drop everything after the first "." (for numerical comparison)
-    version=${version%%.*}
-    # numeric comparison
-    if [ $version -ge $minjavaversion ]
-    then
-      ok=1
-    fi  
-  fi
-  return $ok
-} # javacheck
-
-#-----------------------------------------------------------------------
-# help displays usage information
-#-----------------------------------------------------------------------
-help()
-{
-  echo "Calling syntax"
-  echo "  siardtodb.sh [-h] | <args>"
-  echo "executes $class in $reljar using $rellogprop for logging."
-  echo ""
-  echo "Parameters"
-  echo "  -h          displays usage information"
-  echo "  <args>      see documentation of SiardToDb"
-  echo ""
-  echo "JavaHome:"
-  echo "  In order to find a suitable java executable, the PATH is first"
-  echo " searched for \"java\", then JAVA_HOME."
-  echo ""
-  echo "JavaOpts:"
-  echo "  The environment variable JAVA_OPTS is used as a"
-  echo "  source for additional JAVA options."
-  echo "  E.g. -Xmx1000m or"
-  echo "       -DproxyHost=www-proxy.admin.ch -DproxyPort=8080 or"
-  echo "       -Dcom.sun.xml.ws.transport.http.client.HttpTransportPipe.dump=true"
-  echo ""
-  return 4
-}
-
-#-----------------------------------------------------------------------
-# display error message about java executable
-#-----------------------------------------------------------------------
-error()
-{
-  echo "No valid java executable with version equal or greater than $minjavaversion could be found!"
-  echo "Install the JAVA JRE or indicate correct location using JAVA_HOME!"
-  return 8
-} # error
+java=jre/bin/java
 
 #-----------------------------------------------------------------------
 # executes $class in $reljar.
@@ -129,25 +45,8 @@ execute()
 #-----------------------------------------------------------------------
 if [ "$1" != "-h" ];
 then
-  args="$@"
-  javafind
-  ok=$?
-  if [ $ok -ne 0 ];
-  then
-    javacheck
-    ok=$?
-    # if a suitable java executable was found then execute it
-    if [ $ok -ne 0 ];
-    then
+
       execute
-    else
-      error
-    fi
-  else
-    error
-  fi  
-else
-  help
 fi
 rc=$?
 exit $rc
